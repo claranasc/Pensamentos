a5b9a529488d48e3e554a086417715e1d31a2fa7
Clara Nascimento <clara.nascimento@outlook.com>
2020-07-30 21:40:50 -0300

    Aula 80 - Create the Outlets and Actions to QuotesViewController andSettingsViewController, parte 2

--- Pensamentos/Pensamentos/Controllers/QuotesViewController.swift
+++ Pensamentos/Pensamentos/Controllers/QuotesViewController.swift
@@ -2,29 +2,26 @@
 //  QuotesViewController.swift
 //  Pensamentos
 //
-//  Created by Clara Nascimento on 30/07/20.
-//  Copyright © 2020 Clara Nascimento. All rights reserved.
-//
 
 import UIKit
 
 class QuotesViewController: UIViewController {
 
+    @IBOutlet weak var ivPhoto: UIImageView!
+    @IBOutlet weak var ivPhotoBg: UIImageView!
+    @IBOutlet weak var lbQuote: UILabel!
+    @IBOutlet weak var lbAuthor: UILabel!
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
