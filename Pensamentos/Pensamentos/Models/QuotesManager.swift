73896bf16eca7e423d446d45f396759a722732ae
Clara Nascimento <clara.nascimento@outlook.com>
2020-08-03 20:01:09 -0300

    Aula 82 - Create func getRandomQuote, parte 1

--- Pensamentos/Pensamentos/Models/QuotesManager.swift
+++ Pensamentos/Pensamentos/Models/QuotesManager.swift
@@ -16,7 +16,11 @@ class QuotesManager {
         let jsonData = try! Data(contentsOf: fileURL)
         let jsonDecoder = JSONDecoder()
         quotes = try! jsonDecoder.decode([Quote].self, from: jsonData)
-        
+    }
+    
+    func getRandomQuote() -> Quote {
+        let index = Int(arc4random_uniform(UInt32(quotes.count)))
+        return quotes[index]
     }
     
 }
