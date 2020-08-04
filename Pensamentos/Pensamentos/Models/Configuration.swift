6c177cde58244718c957751a7dc5d5d5cef98438
Clara Nascimento <clara.nascimento@outlook.com>
2020-08-04 20:19:54 -0300

    Aula 83 - Create enum UserDefaultsKeys e várias variáveis in Configuration, parte 1

--- Pensamentos/Pensamentos/Models/Configuration.swift
+++ Pensamentos/Pensamentos/Models/Configuration.swift
@@ -8,6 +8,47 @@
 
 import Foundation
 
+enum UserDefaultsKeys: String {
+    case timeInterval = "timeInterval"
+    case colorScheme = "colorScheme"
+    case autorefresh = "autorefresh"
+}
+
+
 class Configuration {
+    
+    let defaults = UserDefaults.standard
+    static var shared: Configuration = Configuration()
+    
+    var timeInterval: Double {
+        get {
+            return defaults.double(forKey: UserDefaultsKeys.timeInterval.rawValue)
+        }
+        set {
+            defaults.set(newValue, forKey: UserDefaultsKeys.timeInterval.rawValue)
+        }
+    }
+    
+    var colorScheme: Int {
+        get {
+            return defaults.integer(forKey: UserDefaultsKeys.colorScheme.rawValue)
+        }
+        set {
+            defaults.set(newValue, forKey: UserDefaultsKeys.colorScheme.rawValue)
+        }
+    }
+
+    var autorefresh: Bool {
+        get {
+            return defaults.bool(forKey: UserDefaultsKeys.autorefresh.rawValue)
+        }
+        set {
+            defaults.set(newValue, forKey: UserDefaultsKeys.autorefresh.rawValue)
+        }
+    }
+    
+    private init(){
+        
+    }
     
 }
