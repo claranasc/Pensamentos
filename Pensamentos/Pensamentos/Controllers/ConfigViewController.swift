5980565cb374ed12e040063b393665c1152f35e7
Clara Nascimento <clara.nascimento@outlook.com>
2020-08-10 18:50:23 -0300

    Aula 83 - Add let config, func formatView and func changeTimeIntervalLabel, parte 3

--- Pensamentos/Pensamentos/Controllers/ConfigViewController.swift
+++ Pensamentos/Pensamentos/Controllers/ConfigViewController.swift
@@ -15,10 +15,28 @@ class ConfigViewController: UIViewController {
     @IBOutlet weak var scColorScheme: UISegmentedControl!
     @IBOutlet weak var lbTimeInterval: UILabel!
     
+    let config = Configuration.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        formatView()
+    }
+    
+    func formatView() {
+        swAutorefresh.setOn(config.autorefresh, animated: false)
+        slTimeInterval.setValue(Float(config.timeInterval), animated: false)
+        scColorScheme.selectedSegmentIndex = config.colorScheme
+        changeTimeIntervalLabel(with: config.timeInterval)
+    }
+    func changeTimeIntervalLabel(with value: Double) {
+        lbTimeInterval.text = "Mudar após \(value) segundos"
+    }
+
+    
     
     @IBAction func changeAutorefresh(_ sender: UISwitch) {
     }
