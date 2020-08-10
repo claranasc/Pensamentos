8585faffb584bedb6640c877703b8699e97f3893
Clara Nascimento <clara.nascimento@outlook.com>
2020-08-10 20:06:32 -0300

    Aula 85 - Add NotificationCenter.default.post no AppDelegate, no ConfigViewController.swift e no QuotesViewController.swift, parte 1

--- Pensamentos/Pensamentos/AppDelegate.swift
+++ Pensamentos/Pensamentos/AppDelegate.swift
@@ -14,8 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "Refresh"), object: nil)
+    }
 }
+
 
